12e1486fa71f840c44aa0c835aabeaa2515c964c
KyowsAccount <lucasminefort123@gmail.com>
2026-05-15 22:06:57 -0300

    otimização do código + algumas mudanças
    mudanças e otimização
    
    mudaças:
    - gun_base
    - gun
    - thompson
    - etc
    
    simplificação de script:
    - health
    - gun base
    - player moviment

--- scripts/guns/gun_base.gd
+++ scripts/guns/gun_base.gd
@@ -1,40 +1,44 @@
 class_name gun_base extends Node2D
 
 const MIN_DISTANCE = 8
-
 @export var fire_rate = 0.2
 @export var bullet_speed = 1000
 @export var damage = 10
 @export var spread = 0
 var last_time_shot = 0.0
-
 var flip_threshold = 135
+var facing_left := false
 
 var bullet_scene = preload("res://scenes/projectiles/bullet_base.tscn")
 @onready var muzzle : Marker2D = $Marker2D
 
 func _physics_process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
-	var distance_to_mouse = global_position.distance_to(mouse_pos)
+	var direction = mouse_pos - global_position
+
+	if direction.length_squared() <= MIN_DISTANCE * MIN_DISTANCE:
+		return
+
+	look_at(mouse_pos)
+
+	var angle = wrapf(rotation_degrees, 0.0, 360.0)
+
+	# Anti flicker
+	if facing_left:
+		if angle < 125.0 or angle > 325.0:
+			facing_left = false
+	else:
+		if angle > 145.0 and angle < 305.0:
+			facing_left = true
+
+	scale.y = -1 if facing_left else 1
+	position.x = -6 if facing_left else 6
+
+	last_time_shot += delta
+
+	if Input.is_action_pressed("shoot") and last_time_shot >= fire_rate:
+		shoot()
 	
-	if distance_to_mouse > MIN_DISTANCE:
-		look_at(get_global_mouse_position())
-		rotation_degrees = wrap(rotation_degrees, 0, 360)
-		
-		if rotation_degrees > flip_threshold and rotation_degrees < (360-flip_threshold):
-			scale.y = -1
-			position.x = -6
-			flip_threshold = 315
-			
-		elif (rotation_degrees < (360 - flip_threshold) or rotation_degrees > flip_threshold)						:
-			scale.y = 1
-			position.x = 6
-			flip_threshold = 135	
-		
-		last_time_shot += delta
-		if Input.is_action_pressed("shoot") and last_time_shot >= fire_rate:
-			shoot()
-		
 func shoot():
 	last_time_shot = 0.0
 	
@@ -53,6 +57,4 @@ func shoot():
 		
 	
 	#if has_node("shoot_sound"):
-	#	$shoot_sound.play()
-	
-	
+		$shoot_sound.play()
